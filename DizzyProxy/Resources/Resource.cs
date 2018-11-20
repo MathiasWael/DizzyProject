2d0339bdc62bd5c8695854158757ff2facd4af29
Lasse Fisker Olesen <fiskero94@gmail.com>
2018-11-20 10:50:26 +0100

    Resource

--- DizzyProxy/Resources/Resource.cs
+++ DizzyProxy/Resources/Resource.cs
@@ -2,33 +2,31 @@
 using System.Threading.Tasks;
 using DizzyProxy.Exceptions;
 using RestSharp;
+using RestSharp.Deserializers;
 
 namespace DizzyProxy.Resources
 {
     public abstract class Resource
     {
         public static string Token { get; set; }
-        private const string BaseUrl = "http://localhost:4000";
+
+        private readonly string _baseUrl;
+        private JsonDeserializer _deserializer;
 
         public Resource()
         {
-
+            _baseUrl = "http://localhost:4000";
+            _deserializer = new JsonDeserializer();
         }
 
-        private Task<IRestResponse> Execute<T>(IRestRequest request) where T : new()
-        {
-            IRestClient client = new RestClient(BaseUrl);
-            TaskCompletionSource<IRestResponse> taskCompletionSource = new TaskCompletionSource<IRestResponse>();
-            client.ExecuteAsync<T>(request, response => taskCompletionSource.TrySetResult(response));
-            return taskCompletionSource.Task;
-        }
-
-        protected async Task<T> ExecuteAsync<T>(RestRequest request) where T : new()
+        protected T Execute<T>(IRestRequest request) where T : new() => ExecuteAsync<T>(request).Result;
+        protected async Task<T> ExecuteAsync<T>(IRestRequest request) where T : new()
         {
             if (Token != null)
                 request.AddHeader("x-auth-token", Token);
-            
-            IRestResponse response = await Execute<T>(request);
+
+            IRestClient client = new RestClient(_baseUrl);
+            IRestResponse response = await ExecuteRequest<T>(client, request);
 
             if (!response.IsSuccessful)
             {
@@ -39,8 +37,15 @@ namespace DizzyProxy.Resources
                     case HttpStatusCode.InternalServerError: throw new InternalServerErrorException(response.Content);
                 }
             }
+            
+            return _deserializer.Deserialize<T>(response);
+        }
 
-            return new T();
+        private Task<IRestResponse> ExecuteRequest<T>(IRestClient client, IRestRequest request) where T : new()
+        {
+            TaskCompletionSource<IRestResponse> taskCompletionSource = new TaskCompletionSource<IRestResponse>();
+            client.ExecuteAsync<T>(request, response => taskCompletionSource.TrySetResult(response));
+            return taskCompletionSource.Task;
         }
     }
 }
