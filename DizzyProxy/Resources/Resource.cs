b9fd433a546493c2ffaf4abb21c49ccbc142d80f
Lasse Fisker Olesen <fiskero94@gmail.com>
2018-11-20 07:58:12 +0100

    Async

--- DizzyProxy/Resources/Resource.cs
+++ DizzyProxy/Resources/Resource.cs
@@ -1,37 +1,46 @@
-﻿using RestSharp;
-using System.Net;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
 using System.Threading.Tasks;
 using DizzyProxy.Exceptions;
+using RestSharp;
 
 namespace DizzyProxy.Resources
 {
     public abstract class Resource
     {
-        private const string BaseUrl = "https://localhost:4000";
+        public static string Token { get; set; }
+        private const string BaseUrl = "http://localhost:4000";
 
         public Resource()
         {
 
         }
 
-        public T Execute<T>(RestRequest request) where T : new()
+        private Task<IRestResponse> Execute<T>(IRestRequest request) where T : new()
         {
             IRestClient client = new RestClient(BaseUrl);
-            IRestResponse response = client.Execute<T>(request);
+            TaskCompletionSource<IRestResponse> taskCompletionSource = new TaskCompletionSource<IRestResponse>();
+            client.ExecuteAsync<T>(request, response => taskCompletionSource.TrySetResult(response));
+            return taskCompletionSource.Task;
+        }
+
+        protected async Task<T> ExecuteAsync<T>(RestRequest request) where T : new()
+        {
+            if (Token != null)
+                request.AddHeader("x-auth-token", Token);
+            
+            IRestResponse response = await Execute<T>(request);
 
             if (!response.IsSuccessful)
             {
                 switch (response.StatusCode)
                 {
-                    case HttpStatusCode.BadRequest: throw new BadRequestException(); break;
-                    case HttpStatusCode.InternalServerError: throw new BadRequestException(); break;
+                    case HttpStatusCode.BadRequest: throw new BadRequestException(response.Content);
+                    case HttpStatusCode.NotFound: throw new NotFoundException(response.Content);
+                    case HttpStatusCode.InternalServerError: throw new InternalServerErrorException(response.Content);
                 }
             }
-            
+
+            return new T();
         }
     }
 }
