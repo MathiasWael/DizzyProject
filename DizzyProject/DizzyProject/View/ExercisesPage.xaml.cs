cf71014957a9b235cec98f74f6039f739c57bfbe
MathiasWael <mathiaswael@hotmail.dk>
2018-11-20 13:50:51 +0100

    d

--- DizzyProject/DizzyProject/View/ExercisesPage.xaml.cs
+++ DizzyProject/DizzyProject/View/ExercisesPage.xaml.cs
@@ -7,16 +7,27 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using DizzyProject.Model;
+using DizzyProxy.Models;
+using DizzyProject.BusinessLogic;
 
 namespace DizzyProject.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ExercisesPage : ContentPage
 	{
-        //List<Exercise> exercises = new List<Exercise>();
+        private ExerciseController exerciseController; 
+        List<Exercise> exercises;
 		public ExercisesPage ()
 		{
 			InitializeComponent ();
-		}
+
+            exerciseController = new ExerciseController();
+
+            exercises = exerciseController.GetAllExercisesById(1); //temp user id
+
+            ListViewExercises.ItemsSource = exercises;
+
+
+        }
 	}
 }
