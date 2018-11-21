c02709ac7385a37cd39150ca6c3753e3bc7c26f1
Ayis1 <32457997+Ayis1@users.noreply.github.com>
2018-11-21 10:34:21 +0100

    aas

--- DizzyProject/DizzyProject/View/RegisterPatientPage.xaml.cs
+++ DizzyProject/DizzyProject/View/RegisterPatientPage.xaml.cs
@@ -6,9 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using DizzyProxy.Models;
-using Sex = DizzyProxy.Models.Sex;
+using DizzyProxy;
 using DizzyProject.BusinessLogic;
+using DizzyProxy.Models;
 
 namespace DizzyProject.View
 {
@@ -18,7 +18,8 @@ namespace DizzyProject.View
         private DateTime datePicked;
         private Sex sex;
         private Country country;
-        CountryController countryController;
+        private CountryController countryController;
+        private PatientResource patientResource;
         public RegisterPatientPage()
         {
             InitializeComponent();
@@ -61,43 +62,60 @@ namespace DizzyProject.View
             }
         }
 
-        private void Submit_Pressed(object sender, EventArgs e)
-        {            
-            string h = Height.Text;
-            short height = Convert.ToInt16(h);
+        private async Task Submit_PressedAsync(object sender, EventArgs e)
+        {
+            bool success = false;
+            string password1 = Password1.Text;
+            string password2 = Password2.Text;
 
-            string w = Weight.Text;
-            short weight = Convert.ToInt16(w);
-
-            string z = ZipCode.Text;
-            int zipCode = Convert.ToInt32(z);
-
-            string c = City.Text;
-            long city = Convert.ToInt64(c);
-
-            Location location = new Location
+            if(password1 != password2)
             {
-                Address = Address.Text,
-                CityId = city,
-                ZipCode = zipCode,
-                CountryId = country.Id
-            };
-
-            Patient patient = new Patient
+                await DisplayAlert("Password mismatch", "Passwords do not match", "OK");
+            }
+            else
             {
-                Created = DateTime.Now,
-                Updated = DateTime.Now,
-                FirstName = FirstName.Text,
-                LastName = LastName.Text,
-                Email = Email.Text,
-                Phone = PhoneNumber.Text,
-                Weight = weight,
-                Height = height,
-                BirthDate = datePicked,
-                Sex = sex,
-                LocationId = location.Id
-            };
-            //Patients.CreatePatientAsync();
+                success = true;
+            }
+
+            if(success == true)
+            {
+                string h = Height.Text;
+                short height = Convert.ToInt16(h);
+
+                string w = Weight.Text;
+                short weight = Convert.ToInt16(w);
+
+                string z = ZipCode.Text;
+                int zipCode = Convert.ToInt32(z);
+
+                string c = City.Text;
+                long city = Convert.ToInt64(c);
+
+                Location location = new Location
+                {
+                    Address = Address.Text,
+                    CityId = city,
+                    ZipCode = zipCode,
+                    CountryId = country.Id
+                };
+
+                Patient patient = new Patient
+                {
+                    Created = DateTime.Now,
+                    Updated = DateTime.Now,
+                    FirstName = FirstName.Text,
+                    LastName = LastName.Text,
+                    Email = Email.Text,
+                    Phone = PhoneNumber.Text,
+                    Weight = weight,
+                    Height = height,
+                    BirthDate = datePicked,
+                    Sex = sex,
+                    LocationId = location.Id
+                };
+
+                await patientResource.CreatePatientAsync(patient.FirstName, patient.LastName, patient.Email, password1);
+            }
         }
     }
 }
