4f85f1cb8404dedda18144248f2b0a539debf045
MathiasWael <mathiaswael@hotmail.dk>
2018-11-22 09:39:49 +0100

    some dizzy stuff

---
+++ DizzyProject/DizzyProject/BusinessLogic/DizzinessController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DizzyProject.BusinessLogic
+{
+    public class DizzinessController
+    {
+        public void RegisterDizziness()
+        {
+
+        }
+    }
+}
