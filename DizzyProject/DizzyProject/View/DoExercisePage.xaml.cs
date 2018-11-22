09e71249c71d68b4ca669fdd568bd5eb705db756
Ayis1 <32457997+Ayis1@users.noreply.github.com>
2018-11-22 12:48:55 +0100

    DoExercise ViewExercise

---
+++ DizzyProject/DizzyProject/View/DoExercisePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DizzyProxy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DizzyProject.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DoExercisePage : ContentPage
+	{
+        Exercise selectedExercise;
+		public DoExercisePage (Exercise exercise)
+		{
+			InitializeComponent ();
+            selectedExercise = exercise;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            ((Slider)sender).Value = Math.Round(e.NewValue);
+            DizzyLevelLabel.Text = ((Slider)sender).Value.ToString();
+        }
+
+        private async void Save_Pressed(object sender, EventArgs e)
+        {
+            //save logic stump
+            //await Navigation.PushModalAsync(new NavigationPage(new DoExercisePage(selectedExercise)));
+        }
+
+        private async void BackButton_Pressed(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ViewExercisePage(selectedExercise)));
+        }
+    }
+}
