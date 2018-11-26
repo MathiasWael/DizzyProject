4032591e8653f16a864ecf54cb85aa4e02e4fc5b
Ayis1 <32457997+Ayis1@users.noreply.github.com>
2018-11-26 16:35:16 +0100

    lol

--- DizzyProxy/Models/Physiotherapist.cs
+++ DizzyProxy/Models/Physiotherapist.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace DizzyProxy.Models
 {
     public class Physiotherapist : User
     {
+        [JsonProperty("organisation_id")]
         public long DepartmentId { get; set; }
     }
 }
