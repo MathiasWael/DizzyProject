57183c712ab5f7dd0d5ceba05fe9f894bdd1901c
Leiki á Mýrini <1065489@ucn.dk>
2018-11-27 14:33:37 +0100

    register dizziness all the good stuff

--- DizzyProject/DizzyProject/View/DizzyRegisterPage.xaml.cs
+++ DizzyProject/DizzyProject/View/DizzyRegisterPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DizzyProject.BusinessLogic;
+using DizzyProxy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,15 +14,27 @@ namespace DizzyProject.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DizzyRegisterPage : ContentPage
 	{
+        DizzinessController dc = new DizzinessController();
+
 		public DizzyRegisterPage ()
 		{
 			InitializeComponent ();
 		}
-
+        
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             ((Slider)sender).Value = Math.Round(e.NewValue);
             DizzyLevelLabel.Text = ((Slider)sender).Value.ToString();
+        }
+
+        private async void Submit_Pressed(object sender, EventArgs e)
+        {
+            Dizziness d;
+            var answer = await DisplayAlert("Submit", "Are you sure you want to submit your answer?", "Yes", "No");
+            if (answer)
+            {
+                d = await dc.SubmitAsync(Convert.ToInt32(dizzinessValue.Value), Note.Text);
+            }
         }
     }
 }
