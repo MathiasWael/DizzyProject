485b1061e8af6697074004e328776cdd011c1091
Leiki á Mýrini <1065489@ucn.dk>
2018-11-28 14:17:59 +0100

    dizziness test

---
+++ DizzyProxyTests/DizzinessTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DizzyProxy.Models;
+using DizzyProxy.Resources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DizzyProxyTests
+{
+    [TestClass]
+    public class DizzinessTest
+    {
+        [TestMethod]
+        public void CreateDizzinessTest_Successful() //dun work, login giver connection fejl
+        {
+            // Arrange
+            DizzinessResource dc = new DizzinessResource();
+            new LoginResource().CreateLogin("annalarsen@hotmail.com", "Password123");
+
+            // Act
+            Task<Dizziness> td = dc.SubmitAsync(null, 7, "Er svimmel i dag - Anna");
+            Dizziness d = (Dizziness)td.AsyncState;
+
+            // Assert
+            Assert.AreEqual(d.Note, "Er svimmel i dag - Anna");
+            Assert.AreEqual(d.Level, 7);
+        }
+    }
+}
