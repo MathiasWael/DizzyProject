16ceea811cb777ca395c4d798d0dbbec4baae491
MathiasWael <mathiaswael@hotmail.dk>
2018-12-13 13:41:39 +0100

    create/update stepcount

--- DizzyProxy/Resources/StepCountResource.cs
+++ DizzyProxy/Resources/StepCountResource.cs
@@ -1,4 +1,5 @@
 ﻿using DizzyProxy.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -13,6 +14,30 @@ namespace DizzyProxy.Resources
         {
             Request request = new Request(Method.GET, $"patients/{userId}/stepcounts");
             return await ExecuteAsync<List<StepCount>>(request);
+        }
+
+        public StepCount CreateStepCount(long userId, int count, DateTime dateTime)
+            => CreateStepCountAsync(userId, count, dateTime).Result;
+
+        public async Task<StepCount> CreateStepCountAsync(long userId, int count, DateTime dateTime)
+        {
+            Request request = new Request(Method.POST, $"patients/{userId}/stepcounts");
+            request.Body["patient_id"] = userId;
+            request.Body["count"] = count;
+            request.Body["date"] = dateTime.ToString("yyyy-MM-dd");
+            return await ExecuteAsync<StepCount>(request);
+        }
+
+        public StepCount UpdateStepCount(StepCount stepCount)
+            => UpdateStepCountAsync(stepCount).Result;
+
+        public async Task<StepCount> UpdateStepCountAsync(StepCount stepCount)
+        {
+            Request request = new Request(Method.PUT, $"patients/{stepCount.PatientId}/stepcounts/{stepCount.Id}");
+            request.Body["patient_id"] = stepCount.PatientId;
+            request.Body["count"] = stepCount.Count;
+            request.Body["date"] = stepCount.Date.ToString("yyyy-MM-dd");
+            return await ExecuteAsync<StepCount>(request);
         }
     }
 }
--- DizzyProxyTests/StepCountTests.cs
+++ DizzyProxyTests/StepCountTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using DizzyProxy.Models;
 using DizzyProxy.Resources;
+using System;
 
 namespace DizzyProxyTests
 {
@@ -43,6 +44,40 @@ namespace DizzyProxyTests
 
             // Assert
             Assert.AreEqual(2, stepCounts.Count());
+        }
+
+        [TestMethod]
+        public void CreateStepCount_Patient_Successful()
+        {
+            // Arrange
+            Helpers.PatientLogin();
+            long patientId = 1;
+            int count = 4215;
+            DateTime dateTime = new DateTime(2018, 12, 05);
+
+            // Act
+            StepCount stepCount = StepCountResource.CreateStepCount(patientId, count, dateTime);
+
+            // Assert
+            Assert.AreEqual(patientId, stepCount.PatientId);
+            Assert.AreEqual(count, stepCount.Count);
+            Assert.AreEqual(47, stepCount.Id);
+        }
+
+        [TestMethod]
+        public void UpdateStepCount_Patient_Successful()
+        {
+            // Arrange
+            Helpers.PatientLogin();
+            StepCount stepCount = StepCountResource.CreateStepCount(1, 3652, new DateTime(2018, 12, 05));
+            stepCount.Count = 4215;
+
+            // Act
+            stepCount = StepCountResource.UpdateStepCount(stepCount);
+
+            // Assert
+            Assert.AreEqual(4215, stepCount.Count);
+            Assert.AreEqual(47, stepCount.Id);
         }
     }
 }
